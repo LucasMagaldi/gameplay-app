55260689c8771988cee9efa3d397f8c89a6afc68
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-10-13 09:38:24 -0300

    init home page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,41 @@
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { Rajdhani_700Bold, Rajdhani_500Medium } from '@expo-google-fonts/rajdhani'
+import * as SplashScreen from 'expo-splash-screen';
+
 import { SignIn } from './src/screens/sign-in';
+import { StatusBar } from 'react-native';
+import { Home } from './src/screens/home';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
-    <SignIn />
+    <>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Home />
+    </>
   )
 }
--- src/screens/sign-in/index.tsx
+++ src/screens/sign-in/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Text, Image, StatusBar } from "react-native"
+import { View, Text, Image } from "react-native"
 
 import IllustrationImg from '../../assets/illustration.png'
 import { styles } from "./styles"
@@ -8,11 +8,6 @@ import { Button } from "../../components/button"
 export function SignIn() {
     return (
         <View style={styles.container}>
-            <StatusBar 
-                barStyle='light-content'
-                backgroundColor='transparent'
-                translucent
-            />
             <Image 
                 source={IllustrationImg} 
                 style={styles.image}
